f802e0c9d15a1ea1a14365713eb73a74bf29004d
kadzahk <juan427lol@gmail.com>
2022-11-30 17:55:52 -0500

    use 2 for in a return to transpose values of a matrix

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -3,7 +3,8 @@
 
 
 def matrix_transpose(matrix):
-    """all the values in te position 1 of every vector are going to be in the vector 1
+    """all the values in te position 1 of every vector
+    are going to be in the vector 1
     consequently with all vectors depending on the position"""
     return [[matrix[y][x] for y in range(len(matrix))]
-          for x in range(len(matrix[0]))]
+            for x in range(len(matrix[0]))]
