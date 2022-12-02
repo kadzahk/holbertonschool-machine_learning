d277e59cb9f6473e34443e0f3c75e8a263bbc56b
kadzahk <juan427lol@gmail.com>
2022-12-01 22:19:37 -0500

    find the length of a matrix with numpy.darray

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""define a function to use in the main file"""
+
+
+def np_shape(matrix):
+    """function to find the length of a matrix with numpy.darray"""
+    return matrix.shape
