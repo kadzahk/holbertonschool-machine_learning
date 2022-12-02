b4e0b10bd0ca613c8e6ca2fa0f5d376445ee0f6b
kadzahk <juan427lol@gmail.com>
2022-12-01 22:32:59 -0500

    addition, subtraction, multiplication, and divisio

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""define a function to use in the main file"""
+
+
+def np_elementwise(mat1, mat2):
+    """function that make an addition,
+    subtraction, multiplication, and division"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
