283a77acd46d98b36fd47396410a5a53c953d697
kadzahk <juan427lol@gmail.com>
2022-12-01 21:57:08 -0500

    multiplication of matrices

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -3,7 +3,7 @@
 
 
 def mat_mul(mat1, mat2):
-    """multiplication of matrices """
+    """multiplication of matrices"""
     if len(mat1[0]) != len(mat2):
         return None
     return [[sum([mat1[x][y] * mat2[y][row] for y in range(len(mat2))])
