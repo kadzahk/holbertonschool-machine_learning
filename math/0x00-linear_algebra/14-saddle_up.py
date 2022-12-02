6d6e27223d29f2893d96d459fc44b0d67d8bdd35
kadzahk <juan427lol@gmail.com>
2022-12-01 22:42:23 -0500

    return a matrix multiplication with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""define a function to use in the main file"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """function that return a matrix multiplication"""
+    return np.matmul(mat1, mat2)
