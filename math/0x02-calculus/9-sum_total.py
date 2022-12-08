650961eb19b50ed6f00e20a5bcfe81c148c302f5
kadzahk <juan427lol@gmail.com>
2022-12-07 19:01:14 -0500

    calculates a summation for power of 2

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -9,4 +9,4 @@ def summation_i_squared(n):
     series sumation formula for power of 2
     """
     if isinstance(n, int) and n > 0:
-        return int((n * ((2 * n) + 1)) / 6)
+        return int((n / 6) * (n + 1) * (2 * n + 1))
